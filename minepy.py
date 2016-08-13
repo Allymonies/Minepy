a0d1f68019f77ee44245e76482d9b41d1f96526c
luker2009 <lukemay@gmail.com>
2016-08-13 17:45:47 -0400

    More bug fixes + made sendPacket work.

--- minepy.py
+++ minepy.py
@@ -15,22 +15,27 @@ class Connect:
 		self.s.connect((ip,port))
 		self.handshake()
 	def sendPacket(self, id, data = None):
-		if str(type(data)) == "<class 'bytearray'>":
-			self.s.send(bytes((len(data)+len(id),)))
+		if str(type(id)) == "<class 'int'>":
+			id = struct.pack("<i", id)
+		if (data != None and str(type(data)) == "<class 'bytearray'>") or (data == None):
+			if data != None:
+				self.s.send(bytes((len(data)+len(id),)))
+			else:
+				self.s.send(bytes((1,))
 			self.s.send(id)
-			if data:
+			if data != None:
 				self.s.send(data)
 		else:
 			raise TypeError
 	def handshake(self):
 		packet = bytearray()
-		packet_id = 0x00
+		packet_id = struct.pack("<i", 0x00)
 		packet += struct.pack("<i", 210)
-		packet += writeString(writeString(self.ip))
+		packet += writeString(self.ip)
 		packet += struct.pack("!H", self.port)
 		packet += struct.pack("<i", 1)
 		self.sendPacket(packet_id, packet)
-		self.sendPacket(0x00)
+		self.sendPacket(struct.pack("<i", 0x00))
 		response = self.s.recv(16384, 0x40)
 		print(str(response))
 		print(str(response,"utf-8"))
