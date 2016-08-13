dc994d43103c47ec37d584affaa1b64ede9add3d
luker2009 <lukemay@gmail.com>
2016-08-13 19:25:46 -0400

    Debug Mode + Bug fixes maybe pls?

--- minepy.py
+++ minepy.py
@@ -9,7 +9,7 @@ def writeString(toConvert):
 	return result
 
 class Connect:
-	def __init__(self, ip, port):
+	def __init__(self, ip, port, debug = False):
 		self.ip = ip
 		self.port = port
 		self.s = socket.socket()
@@ -19,24 +19,31 @@ class Connect:
 		self.thread.start()
 	def sendPacket(self, id, data = None):
 		if str(type(id)) == "<class 'int'>":
-			id = struct.pack("<i", id)
+			id = struct.pack("!i", id)
 		if (data != None and str(type(data)) == "<class 'bytearray'>") or (data == None):
 			if data != None:
-				self.s.send(bytes((len(data)+len(id),)))
+				if debug:
+					print("Sent: " + ''.join(r'\x' + hex(letter)[2:] for letter in struct.pack("!i", len(data) + len(id))))
+				self.s.send(bytearray((len(data)+len(id),)))
 			else:
+				if debug:
+					print("Sent: " + ''.join(r'\x' + hex(letter)[2:] for letter in bytes((1,))))
 				self.s.send(bytes((1,)))
+			if debug:
+				print("Sent: " + ''.join(r'\x' + hex(letter)[2:] for letter in bytearray(id)))
 			self.s.send(bytearray(id))
 			if data != None:
+				print("Sent: " + ''.join(r'\x' + hex(letter)[2:] for letter in data))
 				self.s.send(data)
 		else:
 			raise TypeError
 	def handshake(self):
 		packet = bytearray()
 		packet_id = b'\x00'
-		packet += struct.pack("<i", 210)
+		packet += struct.pack("!i", 210)
 		packet += writeString(self.ip)
 		packet += struct.pack("!H", self.port)
-		packet += struct.pack("<i", 1)
+		packet += struct.pack("!i", 1)
 		self.sendPacket(packet_id, packet)
 		self.sendPacket(b'\x00')
 	def receive(self):
